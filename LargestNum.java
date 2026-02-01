7bc5ae3f8f77fad0655e4d0247ba0a000a4c03b4
ajinkyapathak-netizen <ajinkyapathak1098@gmail.com>
2026-02-01 19:21:34 +0530

    Add files via upload

---
+++ LargestNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LargestNum {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter three numbers : ");
+		int a =sc.nextInt();
+		int b =sc.nextInt();
+		int c =sc.nextInt();
+
+		if ( a > b ) {
+			if ( a > c ) {
+				System.out.println(a+" is Largest.");
+			} else { 
+				System.out.println(c+" is Largest.");
+			}
+		} else {
+			if ( b > c ) {
+				System.out.println(b+" is Largest.");
+			} else { 
+				System.out.println(c+" is Largest.");
+			}
+		}
+	}
+}
+
+		
+		
